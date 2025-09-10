df01e306aca2436f76e8cee7cb0325c330263ce6
Qi Wang <61755195+NKkinglord@users.noreply.github.com>
2025-09-10 14:55:12 -0400

    Create .gitignore

---
+++ HW/test1.java
@@ -0,0 +1,10 @@
+// Name : Qi Wang
+// qi.wang@emory.edu / 2667904
+// THIS CODE WAS MY OWN WORK , IT WAS WRITTEN WITHOUT
+// CONSULTING ANY SOURCES OUTSIDE OF THOSE APPROVED BY THE INSTRUCTOR .
+// Qi Wang
+public class test1 {
+    public static void main(String[] args) {
+
+    }
+}
