b028403449bc2ece95f1543b17f4df9725de690d
Qi Wang <61755195+NKkinglord@users.noreply.github.com>
2025-09-10 14:06:48 -0400

    Update and rename CodingProblem1 to CodingProblem1.java

---
+++ HW/CodingProblem1.java
@@ -0,0 +1,18 @@
+// Name : Qi Wang
+// qi.wang@emory.edu / 2667904
+// THIS CODE WAS MY OWN WORK, IT WAS WRITTEN WITHOUT
+// CONSULTING ANY SOURCES OUTSIDE OF THOSE APPROVED BY THE INSTRUCTOR .
+// Qi Wang
+public class CodingProblem1 {
+    public static void main(String[] args) {
+        int wholenum = 1234;
+        float floatnum = 99.99f;
+        double doublenum = 100.001;
+        char alpha = 'Z';
+
+        System.out.println(floatnum + doublenum);
+        System.out.println(wholenum + alpha);
+
+
+    }
+}
