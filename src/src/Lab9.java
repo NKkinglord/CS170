d622ba714324113148c561d35e55d7859c70ca4a
Qi Wang <61755195+NKkinglord@users.noreply.github.com>
2025-11-09 17:48:29 -0500

    lab9

--- src/src/Lab9.java
+++ src/src/Lab9.java
@@ -5,23 +5,23 @@
 // Qi Wang
 
 // Test Case 1: matrixAddition
-// Input Matrix 1 : [[2,2,2,2], [4,4,0,0]]
-// Input Matrix 2 : [[2,2,4,4], [1,2,2,0]]
-// Expected Output :[[4, 4, 6, 6], [5, 6, 2, 0]]
-// Actual Output : [[4, 4, 6, 6], [5, 6, 2, 0]]
+// Input Matrix 1 : [[2,2], [4,4]]
+// Input Matrix 2 : [[2,2], [1,0]]
+// Expected Output :[[4, 4], [5, 4]]
+// Actual Output : [[4, 4], [5, 4]]
 // Test Case 1: matrixMultiplication
-// Input Matrix 1 : [[2,2,2,2], [4,4,0,0]]
-// Input Matrix 2 : [[2,2,4,4], [1,2,2,0]]
-// Expected Output :[[4, 4, 8, 8], [4, 8, 0, 0]]
-// Actual Output : [[4, 4, 8, 8], [4, 8, 0, 0]]
+// Input Matrix 1 : [[2,2], [4,4]]
+// Input Matrix 2 : [[2,2], [1,0]]
+// Expected Output :[[6, 4], [12, 8]]
+// Actual Output : [[6, 4], [12, 8]]
 // Test Case 2: matrixAddition
 // Input Matrix 1 : [[2,2,2,2], [4,4,0,0]]
-// Input Matrix 2 : [[2,2,4,4], [1,0,0,0,0]]
+// Input Matrix 2 : [[2,2,4,4,0], [1,0,0,0,0]]
 // Expected Output :null
 // Actual Output : null
 // Test Case 2: matrixMultiplication
 // Input Matrix 1 : [[2,2,2,2], [4,4,0,0]]
-// Input Matrix 2 : [[2,2,4,4], [1,0,0,0,0]]
+// Input Matrix 2 : [[2,2,4,4,0], [1,0,0,0,0]]
 // Expected Output :null
 // Actual Output : null
 import java.util.Arrays;
@@ -40,15 +40,20 @@ public class Lab9{
         return result;
     }
     static int[][] matrixMultiplication(int m1[][], int rows_m1, int cols_m1, int m2[][], int rows_m2, int cols_m2) {
-        if (rows_m1 != rows_m2 || cols_m1 != cols_m2) {
+        if (cols_m1 != rows_m2) {
             return null;
         }
 
-        int[][] result = new int[rows_m1][cols_m1];
+        int[][] result = new int[rows_m1][cols_m2];
 
         for (int i = 0; i < rows_m1; i++) {
-            for (int j = 0; j < cols_m1; j++) {
-                result[i][j] = m1[i][j] * m2[i][j];
+            for (int j = 0; j < cols_m2; j++) {
+                result[i][j] = 0;
+                int sum = 0;
+                for (int k = 0; k < cols_m1; k++) {
+                    sum += m1[i][k] * m2[k][j];
+                }
+                result[i][j] = sum;
             }
         }
         return result;
@@ -56,23 +61,23 @@ public class Lab9{
 
 public static void main(String[] args) {
     int[][] m1 = {
-            {2, 2, 2, 2},
-            {4, 4, 0, 0}
+            {2, 2},
+            {4, 4}
     };
     int[][] m2 = {
-            {2, 2, 4, 4},
-            {1, 2, 2, 0}
+            {2, 2},
+            {1, 0}
     };
     int[][] m3 = {
             {2, 2, 2, 2},
             {4, 4, 0, 0}
     };
     int[][] m4 = {
-            {2, 2, 4, 4},
+            {2, 2, 4, 4, 0},
             {1, 0, 0, 0, 0}
     };
-    System.out.println(Arrays.deepToString(matrixAddition(m1,2,4,m2,2,4)));
-    System.out.println(Arrays.deepToString(matrixMultiplication(m1,2,4,m2,2,4)));
+    System.out.println(Arrays.deepToString(matrixAddition(m1,2,2,m2,2,2)));
+    System.out.println(Arrays.deepToString(matrixMultiplication(m1,2,2,m2,2,2)));
     System.out.println(Arrays.deepToString(matrixAddition(m1,2,4,m3,2,5)));
     System.out.println(Arrays.deepToString(matrixMultiplication(m1,2,4,m4,2,5)));
     }
